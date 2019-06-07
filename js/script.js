e5b612fb4220cccf7102ca92bc453b7cf20685ec
Nicolas <ngreppi@udesa.edu.ar>
2019-06-07 14:07:15 -0300

    reemplace los archivos subidos por el proyecto que yo tenia

--- js/script.js
+++ js/script.js
@@ -26,3 +26,36 @@ if (esDeportePreferido(deportesArray[2], deportesPreferidos)) {
 }
 
 */
+// fetch("https://api.themoviedb.org/3/movie/550?api_key=2816f7e0d88c5c7abbd4003375fc50fe")
+// .then(function(response) {
+// return response.json()
+// })
+// .then(function(information) {
+// console.log(information);
+// })
+// .catch(function(error) {
+// console.log("Error: " + error);
+// })
+
+var api_key = "2816f7e0d88c5c7abbd4003375fc50fe"
+var urlPopulares = "https://api.themoviedb.org/3/movie/popular?api_key="+api_key+"&language=en-US&page=1"
+
+
+fetch(urlPopulares)
+.then(function(response) {
+return response.json()
+})
+.then(function(information) {
+console.log(information);
+
+
+
+// <article class="">
+//     <p>titulo</p>
+//     <img src="" alt="">
+// </article>
+
+})
+.catch(function(error) {
+console.log("Error: " + error);
+})
